fedf646adedd4818e14d5337c848ce69b0c75e51
Jaedon Beckford <jaedon.justin.beckford@gmail.com>
2025-11-11 17:22:43 -0500

    Completed landing page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+    const message = document.querySelector('.message');
+    const email = document.querySelector('#email');
+
+    document.querySelector(".newsletter form").addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        const emailName = email.value.trim();
+        if (emailName === "") {
+            message.textContent = "Please enter a valid email address.";
+        } else {
+            message.textContent = "Thank you! Your email address " + emailName + " has been added to our mailing list!";
+        }
+    });
+};
